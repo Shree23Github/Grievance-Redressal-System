7a49399c1b3b22d02e7ff78e1d9587796ac379de
SHOURYA SHREE <93870060+Shree23Github@users.noreply.github.com>
2024-04-04 19:29:16 +0530

    Commited

--- Backend.py
+++ Backend.py
@@ -1,17 +1,53 @@
 from fastapi import Body,FastAPI
 from fastapi.params import Body
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from typing import Optional
 
 app=FastAPI()
 
-class User():
-    Name:str
-    Userid:str
+class UserIn(BaseModel):
+    userid:str
+    password:str
+    email_id:EmailStr
+    full_name:str|None=None
     
+class UserOut(BaseModel):
+    userid:str
+    email_id:EmailStr
+    full_name:str|None=None
+    
+class UserInDB(BaseModel):
+    userid:str
+    hashed_password:str
+    email:EmailStr
+    full_name:str|None=None
+    
+def fake_password_hasher(raw_password:str):
+    return "supersecret" + raw_password
+
+def fake_save_user(user_in:UserIn):
+    hashed_password=fake_password_hasher(user_in.password)
+    user_in_db=UserInDB(**user_in.dict(),hashed_password=hashed_password)
+    print("User not saved")
+    
+@app.post("/user/",response_model=UserOut)
+async def create_user(user_in:UserIn):
+    user_saved=fake_save_user(user_in)
+    return user_saved
 
 @app.post("/create_complaints")
 def create_complaints():
     return {"message":"complaints created successfully"}
 
-    
+@app.post("/create_suggestions")
+def create_suggestions():
+    return {"message":"suggestions created successfully"}
+
+@app.post("/create_feedback")
+def create_feedback():
+    return {"message":"feedback created successfully"}
+
+@app.get("/getuser")
+def get_user():
+    return {"message":"user"}
+
